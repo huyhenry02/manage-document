7c6191716363234c4c8fb809704c37d7f0f9e378
John <sandbox.duchuy02@gmail.com>
2024-11-23 12:30:38 +0700

    update feedback 20/11

--- resources/views/document/request/request-update-detail.blade.php
+++ resources/views/document/request/request-update-detail.blade.php
@@ -300,6 +300,32 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="rejectedDocumentModal" tabindex="-1" role="dialog"
+         aria-labelledby="rejectedDocumentModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="rejectedDocumentModalLabel">Từ chối yêu cầu</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="{{ route('document.confirmRequest', $documentAction->id) }}" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <input type="hidden" name="action" value="rejected">
+                        <span>
+                            Bạn có chắc chắn muốn từ chối yêu cầu này không?
+                        </span>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+                        <button type="submit" class="btn btn-primary">Xác nhận</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
